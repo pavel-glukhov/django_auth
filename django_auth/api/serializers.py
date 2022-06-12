4774f27281204cdb0636e5e04239949b9dbc6df3
pavel-glukhov <glukhov.p@gmail.com>
2022-06-12 20:24:06 +0600

    Updated Serializers

--- django_auth/api/serializers.py
+++ django_auth/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework.serializers import Serializer, CharField, EmailField, ModelSerializer
-from api.models import User
+from .models import User
 
 
 class UserSerializer(ModelSerializer):
