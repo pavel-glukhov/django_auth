e52669bccc5b3ae11b37212c9063bfa475df4852
pavel-glukhov <glukhov.p@gmail.com>
2022-06-12 20:24:23 +0600

    Added Routes

--- django_auth/api/urls.py
+++ django_auth/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from api import views
+from . import views
 
 api_router = DefaultRouter()
 
@@ -8,6 +8,6 @@ api_router.register('users', views.UserViewSet)
 
 urlpatterns = [
     path('', include(api_router.urls)),
-    path('auth/token/', views.CreateToken.as_view(), name='token'),
+    path('auth/token/', views.CreateTokenView.as_view(), name='token'),
     path('auth/signup/', views.Signup.as_view(), name='signup'),
 ]
