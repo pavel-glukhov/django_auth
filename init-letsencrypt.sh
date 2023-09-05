a5e5bcda5889239c6bbca95628f053d97ce23674
Pavel Glukhov <glukhov.p@gmail.com>
2023-09-05 22:13:12 +0600

    init

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(glu.kozow.com)
+domains=(glu.kozow.com www.glu.kozow.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="glukhov.p@gmail.com" # Adding a valid address is strongly recommended
